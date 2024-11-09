3361fe6c846d4cc1265fe6deb6207fd9ac0ad9de
khvalbotaihor <31129714+khvalbotaihor@users.noreply.github.com>
2024-11-09 11:23:41 +0200

    add create and delete article test

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -10,19 +10,25 @@ test.beforeEach(async({page}) => {
       })      
   })
 
-  await page.route('*/**/api/articles?limit=10&offset=0', async route => {
-    const response = await route.fetch()
-    const responseBody = await response.json()
-    responseBody.articles[0].title = 'this is test title'
-    responseBody.articles[0].description = 'this is test description'
+  // await page.route('*/**/api/articles?limit=10&offset=0', async route => {
+  //   const response = await route.fetch()
+  //   const responseBody = await response.json()
+  //   responseBody.articles[0].title = 'this is test title'
+  //   responseBody.articles[0].description = 'this is test description'
 
-    await route.fulfill({
-        body: JSON.stringify(responseBody)
-    })
-  })
+  //   await route.fulfill({
+  //       body: JSON.stringify(responseBody)
+  //   })
+  // })
 
   await page.goto('https://conduit.bondaracademy.com/')
   await page.waitForTimeout(1000)
+
+  await page.getByText('Sign in').click()
+  await page.getByRole('textbox',{name: 'Email'}).fill('upqode.igor@gmail.com')
+  await page.getByRole('textbox', {name: 'Password'}).fill('upqode')
+  await page.getByRole('button', {name: 'Sign in'}).click()
+
 })
 
 
@@ -33,7 +39,7 @@ test('check component presence', async ({page})=>{
   await expect(page.locator('app-article-preview p').first()).toContainText('this is test description')
 })
 
-test('delete articles', async ({page, request}) => {
+test('create and then delete articles', async ({page, request}) => {
   const response = await request.post('https://conduit-api.bondaracademy.com/api/users/login',{
     data: {"user":{"email":"upqode.igor@gmail.com","password":"upqode"}}
   } )
@@ -41,37 +47,87 @@ test('delete articles', async ({page, request}) => {
   const accessToken = responseBody.user.token
 
   const articleResponse = await request.post('https://conduit-api.bondaracademy.com/api/articles',{
-    data: {"article":{"title":"test234","description":"about","body":"description","tagList":["tag1"]}},
+    data: {"article":{"title":"test2345","description":"about","body":"description","tagList":["tag1"]}},
     headers: {
       Authorization: `Token ${accessToken}`
     }
   } )
-  await page.waitForTimeout(5000)
+  await page.waitForTimeout(500)
 
-  //await expect(page.locator('app-article-list app-article-preview', {hasText: 'test234'})).toBeVisible()
-  await expect(articleResponse.status()).toBe(201)
+   const articleBody = await articleResponse.json()
+   const articleId = articleBody.article.slug
+   await expect(articleResponse.status()).toBe(201)
   
-  const deleteF = await request.delete('https://conduit-api.bondaracademy.com/api/articles/test234-12473',{
-    //data: {"article":{"title":"test234","description":"about","body":"description","tagList":["tag1"]}},
+  const deleteFunction = await request.delete(`https://conduit-api.bondaracademy.com/api/articles/${articleId}`,{
     headers: {
       Authorization: `Token ${accessToken}`
     }
   } )
-  await expect(articleResponse.status()).toBe(200)
+  await expect(deleteFunction.status()).toBe(204)
+  await page.waitForTimeout(500)
 
+
+  // add new note by post request and then delete this note manually
+  const articleResponse2 = await request.post('https://conduit-api.bondaracademy.com/api/articles',{
+    data: {"article":{"title":"test23456","description":"about","body":"description","tagList":["tag1"]}},
+    headers: {
+      Authorization: `Token ${accessToken}`
+    }
+  } )
+  await expect(articleResponse2.status()).toBe(201)
+
+  await page.getByText('Your Feed').click()
+  await page.getByText('Global Feed').click()
+
+  await page.getByText('test23456').click()
+  await page.getByRole('button', {name:' Delete Article'}).first().click()
+
+  const headers = await page.locator('article-page h1').allTextContents();
+  headers.forEach(text => {
+      expect(text).not.toContain('New Article');
+  });
 })
 
-test('create article', async ({page}) =>{
+test('create article', async ({page, request}) =>{
   await page.getByText('New Article').click()
-  await page.getByRole('textbox', {name: 'New Article'}).fill('title')
+  await page.getByRole('textbox', {name: 'Article title'}).fill('title 222')
   await page.getByRole('textbox', {name: "What's this article about?"}).fill('descriptiom')
   await page.getByRole('textbox', {name: 'Write your article (in markdown)'}).fill('it is a long text')
   await page.getByRole('textbox', {name: 'Enter tags'}).fill('tag 1')
   await page.getByRole('button', {name: 'Publish Article'}).click()
 
-  await expect(page.locator('article-page h1')).toHaveText('New Article')
+  const response = await page.waitForResponse(response => 
+    response.url().includes('api/articles') && response.status() == 201
+  )
+  const jsonResponse  = await response.json()
+  const articleId = jsonResponse.article.slug
+
+  await expect(page.locator('app-article-page h1')).toHaveText('title 222')
 
   await page.getByText('Home').click()
-  await expect(page.locator('app-article-preview h1').first()).toContainText('New Article')
+  await expect(page.locator('app-article-preview h1').first()).toContainText('title 222')
+//
+  const loginResponse = await request.post('https://conduit-api.bondaracademy.com/api/users/login',{
+    data: {"user":{"email":"upqode.igor@gmail.com","password":"upqode"}}
+  } )
+  const responseBody = await loginResponse.json()
+  const accessToken = responseBody.user.token
+
+  // delete item
+  const deleteFunction = await request.delete(`https://conduit-api.bondaracademy.com/api/articles/${articleId}`,{
+    headers: {
+      Authorization: `Token ${accessToken}`
+    }
+  } )
+  await expect(deleteFunction.status()).toBe(204)
+
+// check deleted item
+  await page.getByText('Your Feed').click()
+  await page.getByText('Global Feed').click()
+
+  const headers = await page.locator('article-page h1').allTextContents();
+  headers.forEach(text => {
+      expect(text).not.toContain('title 222');
+  });
 
 })
