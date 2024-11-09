fd3b37b508ab4dd2486566726f6783586b4772ba
khvalbotaihor <31129714+khvalbotaihor@users.noreply.github.com>
2024-11-09 17:56:24 +0200

    refactor to api authentification

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -5,16 +5,14 @@ import fs from 'fs'
 const authFile = './auth/user.json'
 
 setup('authentication', async({request}) => {
+    // authentification through UI
     // await page.goto('https://conduit.bondaracademy.com/')  
     // await page.getByText('Sign in').click()
     // await page.getByRole('textbox',{name: 'Email'}).fill('upqode.igor@gmail.com')
     // await page.getByRole('textbox', {name: 'Password'}).fill('upqode')
     // await page.getByRole('button', {name: 'Sign in'}).click()
     // await page.waitForResponse('https://conduit-api.bondaracademy.com/api/tags')
-
     // await page.context().storageState({ path: authFile })
-
-
 
     const response = await request.post('https://conduit-api.bondaracademy.com/api/users/login',{
         data: {"user":{"email":"upqode.igor@gmail.com","password":"upqode"}}
