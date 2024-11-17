422b67ac815624d17042e8604321193072925613
khvalbotaihor <31129714+khvalbotaihor@users.noreply.github.com>
2024-11-17 18:18:28 +0100

    update

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,15 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 16,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    process.env.CI ? ["dot"] : ["list"],
+    ['html'],
+    [ "@argos-ci/playwright/reporter",
+      {
+        // Upload to Argos on CI only.
+        uploadToArgos: !!process.env.CI,
+      },]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -30,6 +38,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
     extraHTTPHeaders: {
       'Authorization': `Token ${process.env.ACCESS_TOKEN}`
     }
--- tests/likesCounter.spec.ts
+++ tests/likesCounter.spec.ts
@@ -1,10 +1,13 @@
 import {test, expect} from '@playwright/test'
+import {argosScreenshot} from '@argos-ci/playwright'
 
 test('Like counter', async({page})=>{
     await page.goto('https://conduit.bondaracademy.com/')
     await page.getByText('Global Feed').click()
     const firstLikeButton = page.locator('app-article-preview').first().locator('button')
     await expect(firstLikeButton).toContainText('0', { timeout: 5000 })
+    await argosScreenshot(page, "not marked like button");
     await firstLikeButton.click()
+    await argosScreenshot(page, "marked like button");
     await expect(firstLikeButton).toContainText('1' ,{ timeout: 5000 })
 })
